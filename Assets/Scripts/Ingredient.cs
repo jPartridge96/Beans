bfc30130aa27d5a23ccb85747055ce02a1a5745d
Hyunjin Kim <117606232+icecreamp@users.noreply.github.com>
2023-02-28 14:36:55 -0500

    Add comment

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,7 +1,18 @@
+/*
+Ingredient.cs
+Created date: 2.28.2023
+Created by: Jordan partridge
+Comment added by: Hyunjin Kim
+ */
+
 using UnityEngine;
 
+/// <summary>
+/// Script for ingredients
+/// </summary>
 public class Ingredient : MonoBehaviour
 {
+    // Name of the ingredient
     public string Name;
     public Ingredient(string name)
     {
--- Assets/Scripts/Recipe.cs
+++ Assets/Scripts/Recipe.cs
@@ -1,10 +1,23 @@
+/*
+Recipe.cs
+Created date: 2.28.2023
+Created by: Jordan partridge
+Comment added by: Hyunjin Kim
+ */
+
 using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// Script for recipes
+/// </summary>
 public class Recipe : MonoBehaviour
 {
+    // Array for cups
     public string[] cups = {"cup1", "cup2", "cup3"};
+    // List for ingredients
     public List<Ingredient> ingredients;
+    // Check if iced drink
     public bool isIced;
 }
 
