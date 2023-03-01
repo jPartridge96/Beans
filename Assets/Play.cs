b12df802c6b2bb4b18ad3771862ffdb85e011d44
Cloudiiiii <Yusufhafeji33@gmail.com>
2023-03-01 17:11:44 -0500

    Added menu scene
    Changed scene order for build
    Added scripts for play and exit button functions

---
+++ Assets/Play.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
